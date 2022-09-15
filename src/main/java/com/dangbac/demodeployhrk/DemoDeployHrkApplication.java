58c3e09d7a7e200619eaac6b74b8fcb8011cacf3
CabIsMe <dangbac872001@gmail.com>
2022-09-15 15:07:58 +0700

    make it better

---
+++ src/main/java/com/dangbac/demodeployhrk/DemoDeployHrkApplication.java
@@ -0,0 +1,13 @@
+package com.dangbac.demodeployhrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeployHrkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDeployHrkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dangbac/demodeployhrk/DemoDeployHrkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangbac.demodeployhrk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeployHrkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
